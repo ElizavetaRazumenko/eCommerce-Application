c10b61281f481ae941b249fe2f2e66c904c32dc7
Uladimir Hrydzin <hrydzin@gmail.com>
2023-08-06 14:43:21 +0400

    refactor: rename folder '2.main page' to '2.MainPage' and import React to header, main, footer

---
+++ src/pages/2. MainPage/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Footer } from '../2. MainPage/components/footer';
+import { Header } from '../2. MainPage/components/header';
+import { Main } from '../2. MainPage/components/main';
+
+export function Init() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
