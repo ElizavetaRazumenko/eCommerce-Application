33665ef587c47d26d33a4a6b939c37fbd65bac58
Elizaveta Razumenko <razumenko99@mail.ru>
2023-08-07 15:17:43 +0300

    feat: fix footer styles

--- src/pages/globalComponents/footer/components/questions/questions.tsx
+++ src/pages/globalComponents/footer/components/questions/questions.tsx
@@ -4,12 +4,14 @@ const Question = () => {
   return (
     <div className={s.question_wrapper}>
       <h3>For questions</h3>
-      <a className={s.email} href='mailto:pizza_italiano@food.com'>
-        pizza_italiano@food.com
-      </a>
-      <a className={s.contact} href='tel:+37539125621548'>
-        +39 12 562 15 48
-      </a>
+      <div className={s.links_wrapper}>
+        <a className={s.email} href='mailto:pizza_italiano@food.com'>
+          pizza_italiano@food.com
+        </a>
+        <a className={s.contact} href='tel:+37539125621548'>
+          +39 12 562 15 48
+        </a>
+      </div>
     </div>
   );
 };
