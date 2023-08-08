6ba1aeaf2c5a02d22dd8553aeab5850a9708fbd2
Elizaveta Razumenko <razumenko99@mail.ru>
2023-08-08 17:23:03 +0300

    feat: add routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import s from './App.module.css';
 
 import LoginPage from '../pages/1.LoginPage/login/login';
-// import Main from '../pages/2.MainPage/main';
+import Main from '../pages/2.MainPage/main';
 import Footer from '../pages/globalComponents/footer/footer';
 import Header from '../pages/globalComponents/header/header';
 
 const App = () => {
   return (
-    <div className={s.container}>
-      <Header />
-      {/* <Main />  */}
-      <LoginPage />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className={s.container}>
+        <Header />
+        <Routes>
+          <Route path='/login' Component={LoginPage} />
+          <Route path='/' Component={Main} />
+        </Routes>
+
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 };
 
--- src/pages/globalComponents/header/components/nav/Nav.tsx
+++ src/pages/globalComponents/header/components/nav/Nav.tsx
@@ -1,14 +1,51 @@
+import { NavLink } from 'react-router-dom';
+
 import s from './nav.module.css';
 
 const Nav = () => {
   return (
     <nav>
       <ul className={s.navigation_list}>
-        <li className={s.nav_item + ' ' + s.no_active_link}>Main</li>
-        <li className={s.nav_item}>Catalog</li>
-        <li className={s.nav_item}>Profile</li>
-        <li className={s.nav_item}>Cart</li>
-        <li className={s.nav_item}>Login page</li>
+        <li className={s.nav_item}>
+          <NavLink
+            to='/'
+            className={({ isActive }) => (isActive ? s.link + ' ' + s.no_active_link : s.link)}
+          >
+            Main
+          </NavLink>
+        </li>
+        <li className={s.nav_item}>
+          <NavLink
+            to='/catalog'
+            className={({ isActive }) => (isActive ? s.link + ' ' + s.no_active_link : s.link)}
+          >
+            Catalog
+          </NavLink>
+        </li>
+        <li className={s.nav_item}>
+          <NavLink
+            to='/profile'
+            className={({ isActive }) => (isActive ? s.link + ' ' + s.no_active_link : s.link)}
+          >
+            Profile
+          </NavLink>
+        </li>
+        <li className={s.nav_item}>
+          <NavLink
+            to='/cart'
+            className={({ isActive }) => (isActive ? s.link + ' ' + s.no_active_link : s.link)}
+          >
+            Cart
+          </NavLink>
+        </li>
+        <li className={s.nav_item}>
+          <NavLink
+            to='/login'
+            className={({ isActive }) => (isActive ? s.link + ' ' + s.no_active_link : s.link)}
+          >
+            Login page
+          </NavLink>
+        </li>
       </ul>
     </nav>
   );
