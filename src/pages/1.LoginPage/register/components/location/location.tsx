25fc1b76321efe1ddc40baf4b8346fdd8fe47c52
Elizaveta Razumenko <razumenko99@mail.ru>
2023-08-10 16:20:08 +0300

    feat: add styles

--- src/pages/1.LoginPage/register/components/location/location.tsx
+++ src/pages/1.LoginPage/register/components/location/location.tsx
@@ -1,21 +1,23 @@
+import React from 'react';
+
 import s from './location.module.scss';
 
 import imageurl from '../../../../../assets/png/downArrow.png';
 
 const Location = () => {
+  const locationWrapperRef = React.useRef<HTMLDivElement>(null);
+  const locationRef = React.useRef<HTMLDivElement>(null);
+
+  const toggleLocation = () => {
+    locationRef.current?.classList.toggle(s.active);
+    locationWrapperRef.current?.classList.toggle(s.hidden);
+  };
   return (
-    <div
-      className={s.location_selector}
-      onClick={(event) => {
-        const trgt = event.target as HTMLElement;
-        const element = trgt.closest(`.${s.location_selector}`);
-        if (element) {
-          element.classList.toggle(s.active);
-        }
-      }}
-    >
-      <p className={s.content}>Select address</p>
-      <img src={imageurl} alt='arrow' className={s.arrow} />
+    <div className={s.location_wrapper + ' ' + s.hidden} ref={locationWrapperRef}>
+      <div className={s.location_select} ref={locationRef} onClick={toggleLocation}>
+        <p className={s.content}>Select address</p>
+        <img src={imageurl} alt='arrow' className={s.arrow} />
+      </div>
     </div>
   );
 };
