23a33532a861680a218c9e388affd7eb925f74b5
Uladimir Hrydzin <hrydzin@gmail.com>
2023-08-15 09:25:34 +0400

    feat: finish adaptive  main without footer

--- src/pages/globalComponents/header/components/nav/Nav.tsx
+++ src/pages/globalComponents/header/components/nav/Nav.tsx
@@ -1,53 +1,77 @@
+import React, { useState } from 'react';
+
 import { NavLink } from 'react-router-dom';
 
 import s from './nav.module.scss';
 
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleBurgerMenu = () => {
+    if (isOpen) {
+      setIsOpen(false);
+    } else {
+      setIsOpen(true);
+    }
+  };
   return (
-    <nav>
-      <ul className={s.navigation_list}>
-        <li className={s.nav_item}>
-          <NavLink
-            to='/'
-            className={({ isActive }) => (isActive ? s.link + ' ' + s.no_active_link : s.link)}
-          >
-            Main
-          </NavLink>
-        </li>
-        <li className={s.nav_item}>
-          <NavLink
-            to='/catalog'
-            className={({ isActive }) => (isActive ? s.link + ' ' + s.no_active_link : s.link)}
-          >
-            Catalog
-          </NavLink>
-        </li>
-        <li className={s.nav_item}>
-          <NavLink
-            to='/profile'
-            className={({ isActive }) => (isActive ? s.link + ' ' + s.no_active_link : s.link)}
-          >
-            Profile
-          </NavLink>
-        </li>
-        <li className={s.nav_item}>
-          <NavLink
-            to='/cart'
-            className={({ isActive }) => (isActive ? s.link + ' ' + s.no_active_link : s.link)}
-          >
-            Cart
-          </NavLink>
-        </li>
-        <li className={s.nav_item}>
-          <NavLink
-            to='/login'
-            className={({ isActive }) => (isActive ? s.link + ' ' + s.no_active_link : s.link)}
-          >
-            Login
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <nav>
+        <ul className={`${s.navigation_list} ${isOpen ? s.burger_menu_open : ''}`}>
+          <li className={s.nav_item}>
+            <NavLink
+              to='/'
+              onClick={() => setIsOpen(false)}
+              className={({ isActive }) => (isActive ? s.link + ' ' + s.no_active_link : s.link)}
+            >
+              Main
+            </NavLink>
+          </li>
+          <li className={s.nav_item}>
+            <NavLink
+              to='/catalog'
+              onClick={() => setIsOpen(false)}
+              className={({ isActive }) => (isActive ? s.link + ' ' + s.no_active_link : s.link)}
+            >
+              Catalog
+            </NavLink>
+          </li>
+          <li className={s.nav_item}>
+            <NavLink
+              to='/profile'
+              onClick={() => setIsOpen(false)}
+              className={({ isActive }) => (isActive ? s.link + ' ' + s.no_active_link : s.link)}
+            >
+              Profile
+            </NavLink>
+          </li>
+          <li className={s.nav_item}>
+            <NavLink
+              to='/cart'
+              onClick={() => setIsOpen(false)}
+              className={({ isActive }) => (isActive ? s.link + ' ' + s.no_active_link : s.link)}
+            >
+              Cart
+            </NavLink>
+          </li>
+          <li className={s.nav_item}>
+            <NavLink
+              to='/login'
+              onClick={() => setIsOpen(false)}
+              className={({ isActive }) => (isActive ? s.link + ' ' + s.no_active_link : s.link)}
+            >
+              Login
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <div className={`${s.burger_btn}`} onClick={toggleBurgerMenu}>
+        <span className={`${s.burger_line} ${s.burger_line_1} ${isOpen ? s.open_line : ''}`}></span>
+        <span className={`${s.burger_line} ${s.burger_line_2} ${isOpen ? s.open_line : ''}`}></span>
+        <span className={`${s.burger_line} ${s.burger_line_3} ${isOpen ? s.open_line : ''}`}></span>
+        <span className={`${s.burger_line} ${s.burger_line_4} ${isOpen ? s.open_line : ''}`}></span>
+      </div>
+    </div>
   );
 };
 
