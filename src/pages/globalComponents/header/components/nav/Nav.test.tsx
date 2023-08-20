e78b6e1d8e504c7a8a02f59c385919f9eca37972
Uladimir Hrydzin <hrydzin@gmail.com>
2023-08-20 06:46:06 +0400

    feat: fimish tests for header and footer

---
+++ src/pages/globalComponents/header/components/nav/Nav.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react';
+
+import React from 'react';
+
+import { MemoryRouter } from 'react-router-dom';
+
+import Nav from './Nav';
+
+describe('Nav component', () => {
+  test('renders correctly', () => {
+    const mockProps = {
+      userState: 'Login',
+      setUserState: jest.fn(),
+    };
+
+    render(
+      <MemoryRouter>
+        <Nav {...mockProps} />
+      </MemoryRouter>,
+    );
+    expect(screen.getByText('Main')).toBeInTheDocument();
+    expect(screen.getByText('Catalog')).toBeInTheDocument();
+    expect(screen.getByText('Details')).toBeInTheDocument();
+    expect(screen.getByText('Profile')).toBeInTheDocument();
+    expect(screen.getByText('Cart')).toBeInTheDocument();
+    expect(screen.getByText('Login')).toBeInTheDocument();
+  });
+});
