8bfe762002bae0a019cb4e86468774794dffb570
Uladimir Hrydzin <hrydzin@gmail.com>
2023-08-20 15:02:32 +0400

    feat: add some changes

--- src/pages/globalComponents/footer/components/location/location.test.tsx
+++ src/pages/globalComponents/footer/components/location/location.test.tsx
@@ -27,12 +27,12 @@ describe('Location component', () => {
 
     expect(link1).toHaveAttribute(
       'href',
-      'https://yandex.by/maps/10445/rome/search/Rome%2C%20st.%20Via%20Tuscolana%2C%20225/?ll=12.533407%2C41.869911&sll=27.555691%2C53.902735&sspn=0.471725%2C0.179023&z=14'
+      'https://yandex.by/maps/10445/rome/search/Rome%2C%20st.%20Via%20Tuscolana%2C%20225/?ll=12.533407%2C41.869911&sll=27.555691%2C53.902735&sspn=0.471725%2C0.179023&z=14',
     );
 
     expect(link2).toHaveAttribute(
       'href',
-      'https://yandex.by/maps/10445/rome/house/ZUsYcwFnS0IPQFprfXR5eXVkZA==/?ll=12.567278%2C41.888970&z=17'
+      'https://yandex.by/maps/10445/rome/house/ZUsYcwFnS0IPQFprfXR5eXVkZA==/?ll=12.567278%2C41.888970&z=17',
     );
   });
 });
