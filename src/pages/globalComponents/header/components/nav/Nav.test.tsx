b6159dacdbc99c5d19ff9efd322c06604846b57f
Uladimir Hrydzin <hrydzin@gmail.com>
2023-08-20 22:09:24 +0400

    fix: dependencies

--- src/pages/globalComponents/header/components/nav/Nav.test.tsx
+++ src/pages/globalComponents/header/components/nav/Nav.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
 
+import '@testing-library/jest-dom/extend-expect';
+
 import React from 'react';
 
 import { MemoryRouter } from 'react-router-dom';
