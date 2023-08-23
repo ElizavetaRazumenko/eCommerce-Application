f8f51ba40937ddba2dc1fdaf0c525e7aec3e9eb6
Elizaveta Razumenko <razumenko99@mail.ru>
2023-08-23 17:28:26 +0300

    feat: start the detailed page realization

--- src/pages/4.DetailedPage/detailedPage.tsx
+++ src/pages/4.DetailedPage/detailedPage.tsx
@@ -2,8 +2,19 @@ import s from './detailedPage.module.scss';
 
 const DetailedPage = () => {
   return (
-    <div className={s.profile_wrapper}>
-      <p className={s.welcome}>Welcome to the Detailed Page!</p>
+    <div className={s.details_wrapper}>
+      <div className={s.picture_wrapper}>
+        <div className={s.pic}>
+          <div className={s.big_img + ' ' + s.pizza}></div>
+        </div>
+        <div className={s.pic_small_left}>
+          <div className={s.small_left_img + ' ' + s.products}></div>
+        </div>
+        <div className={s.pic_small_right}>
+          <div className={s.small_right_img + ' ' + s.box}></div>
+        </div>
+      </div>
+      <div className={s.description_wrapper}></div>
     </div>
   );
 };
