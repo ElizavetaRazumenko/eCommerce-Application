86178cde3955243231416c402bb275844eeb8109
Uladimir Hrydzin <hrydzin@gmail.com>
2023-08-26 08:38:40 +0400

    fix: page Catalog

--- src/pages/3.CatalogPage/components/catalog-pizzas/catalog-pizza/pizza.tsx
+++ src/pages/3.CatalogPage/components/catalog-pizzas/catalog-pizza/pizza.tsx
@@ -1,20 +1,13 @@
-import React, { useState } from 'react';
-
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import s from './pizza.module.scss';
-
-import infoProducts from '../../../../../entities/product';
 
 import { PizzaType } from '../../../../../types/types';
 
 import PizzaParams from '../catalog-pizzaParams/pizzaParams';
 
-const Pizza = (props: PizzaType & { index: number }) => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  const pizzaIndex = props.index;
-
+const Pizza = (props: PizzaType) => {
   const arrayPizzaParams = [
     {
       size: 'L',
@@ -35,43 +28,108 @@ const Pizza = (props: PizzaType & { index: number }) => {
 
   const pizzaSizes = ['large', 'middle', 'small'];
 
-  const mainIngredients = infoProducts.pizzas[pizzaIndex].mainIngredients;
-
   return (
     <div className={s.pizza_item_catalog}>
-      <div
-        className={`${s.blur_wrapper} ${isHovered ? s.hovered : ''}`}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-      >
-        <div className={s.image_wrapper}>
-          <img src={props.link} className={s.pizza_img} alt='pizza' />
-          <div className={s.overlay}></div>
-        </div>
+      <div className={s.pizza_content}>
+        <img src={props.link} className={s.pizza_img} alt='pizza' />
         <h3 className={s.pizza_name}>{props.name}</h3>
-        <div className={s.pizza_ingredients}>{mainIngredients}</div>
-      </div>
-      <div className={s.pizza_params}>
-        {props.cost.map((price, index) => (
-          <PizzaParams
-            size={arrayPizzaParams[index].size}
-            length={arrayPizzaParams[index].length}
-            price={price}
-            key={index}
-          />
-        ))}
-      </div>
-      {isHovered && (
+        <div className={s.pizza_ingredients}>{props.mainIngredients}</div>
         <div className={s.size_links}>
           {pizzaSizes.map((size, index) => (
             <NavLink to='/detailed' className={s.size_link} key={index}>
-              {size}
+              {pizzaSizes[index]}
             </NavLink>
           ))}
         </div>
-      )}
+      </div>
+      <div className={s.pizza_params}>
+        {arrayPizzaParams.map((pizza, index) => (
+          <PizzaParams size={pizza.size} length={pizza.length} price={pizza.price} key={index} />
+        ))}
+      </div>
     </div>
   );
 };
 
 export default Pizza;
+
+// import React, { useState } from 'react';
+
+// import { NavLink } from 'react-router-dom';
+
+// import s from './pizza.module.scss';
+
+// import { PizzaType } from '../../../../../types/types';
+// import PizzaParams from '../catalog-pizzaParams/pizzaParams';
+
+// const Pizza = (props: PizzaType & { index: number }) => {
+//   const [isHovered, setIsHovered] = useState(false);
+//   let hoverTimeout: NodeJS.Timeout;
+
+//   const pizzaIndex = props.index;
+
+//   const arrayPizzaParams = [
+//     {
+//       size: 'L',
+//       length: '45cm',
+//       price: props.cost[0],
+//     },
+//     {
+//       size: 'M',
+//       length: '30cm',
+//       price: props.cost[1],
+//     },
+//     {
+//       size: 'S',
+//       length: '25cm',
+//       price: props.cost[2],
+//     },
+//   ];
+
+//   const pizzaSizes = ['large', 'middle', 'small'];
+
+//   return (
+//     <div className={s.pizza_item_catalog}>
+//       <div
+//         className={`${s.hoverable} ${isHovered ? s.hovered : ''}`}
+//         onMouseEnter={() => {
+//           setIsHovered(true);
+//           clearTimeout(hoverTimeout);
+//         }}
+//         onMouseLeave={() => {
+//           hoverTimeout = setTimeout(() => setIsHovered(false), 200);
+//         }}
+//       >
+//         <div className={s.image_wrapper}>
+//           <img src={props.link} className={s.pizza_img} alt='pizza' />
+//           <div className={s.overlay}></div>
+//         </div>
+//         <h3 className={s.pizza_name}>{props.name}</h3>
+//         <div className={`${s.pizza_ingredients} ${isHovered ? s.visible : ''}`}>
+//           {props.mainIngredients}
+//         </div>
+//       </div>
+//       <div className={s.pizza_params}>
+//         {props.cost.map((price, index) => (
+//           <PizzaParams
+//             size={arrayPizzaParams[index].size}
+//             length={arrayPizzaParams[index].length}
+//             price={price}
+//             key={index}
+//           />
+//         ))}
+//       </div>
+//       {isHovered && (
+// <div className={s.size_links}>
+//   {pizzaSizes.map((size, index) => (
+//     <NavLink to='/detailed' className={s.size_link} key={index}>
+//       {size}
+//     </NavLink>
+//   ))}
+// </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Pizza;
--- src/pages/3.CatalogPage/components/inputs/inputs.tsx
+++ src/pages/3.CatalogPage/components/inputs/inputs.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent } from 'react';
+import React, { useState, useRef } from 'react';
 
 import s from './inputs.module.scss';
 
@@ -6,55 +6,137 @@ const Inputs = () => {
   const [sortBy, setSortBy] = useState('');
   const [findBy, setFindBy] = useState('');
   const [filterBy, setFilterBy] = useState('');
+  const [isSortOpen, setIsSortOpen] = useState(false);
 
-  const sortChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setSortBy(event.target.value);
+  // const sortChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  //   setSortBy(event.target.value);
+  // };
+  // const findChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setFindBy(event.target.value);
+  // };
+  // const filterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setFilterBy(event.target.value);
+  // };
+
+  const toggleSort = () => {
+    setIsSortOpen(!isSortOpen);
   };
 
-  const findChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setFindBy(event.target.value);
-  };
-
-  const filterChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setFilterBy(event.target.value);
+  const handleSortClick = (value: string) => {
+    setSortBy(value);
+    setIsSortOpen(false);
   };
 
   return (
     <div className={s.inputs_wrapper}>
-      <div className={s.inputs}>
-        <div className={s.input_wrapper}>
-          <input
-            type='text'
-            placeholder='Sort by'
-            value={sortBy}
-            onChange={sortChange}
-            className={s.input + ' ' + s.input_sort}
-          />
-          <div className={s.input_sort_after}></div>
-        </div>
-        <div className={s.input_wrapper}>
-          <input
-            type='text'
-            placeholder='Find'
-            value={findBy}
-            onChange={findChange}
-            className={s.input + ' ' + s.input_find}
-          />
-          <div className={s.input_find_after}></div>
-        </div>
-        <div className={s.input_wrapper}>
-          <input
-            type='text'
-            placeholder='Filter by'
-            value={filterBy}
-            onChange={filterChange}
-            className={s.input + ' ' + s.input_filter}
-          />
-          <div className={s.input_filter_after}></div>
-        </div>
+      <div
+        onClick={toggleSort}
+        className={`${s.input} ${s.input_sort} ${isSortOpen ? s.open_sort_select : ''}`}
+      >
+        <div className={s.sort_title}> {sortBy ? sortBy : 'Sort by'}</div>
+        {isSortOpen && (
+          <ul className={s.sort_list}>
+            <li
+              className={`${s.sort_line} ${s.sort_line_up}`}
+              onClick={() => handleSortClick('Low to High')}
+            >
+              price (Low to High)
+            </li>
+            <li
+              className={`${s.sort_line} ${s.sort_line_down}`}
+              onClick={() => handleSortClick('High to Low')}
+            >
+              price (High to Low)
+            </li>
+            <li className={`${s.sort_line} ${s.sort_az}`} onClick={() => handleSortClick('A to Z')}>
+              alphabet (A to Z)
+            </li>
+            <li className={`${s.sort_line} ${s.sort_za}`} onClick={() => handleSortClick('Z to A')}>
+              alphabet (Z to A)
+            </li>
+          </ul>
+        )}
       </div>
+
+      {/* <div className={s.input_wrapper}> */}
+      {/* <div className={`${s.input} ${s.input_find}`}> */}
+      <input
+        type='text'
+        placeholder='Find'
+        value={findBy}
+        onChange={findChange}
+        className={`${s.input} ${s.input_find}`}
+      />
+      <input
+        type='text'
+        placeholder='Filter by'
+        value={filterBy}
+        onChange={filterChange}
+        className={`${s.input} ${s.input_filter}`}
+      />
     </div>
   );
 };
 
 export default Inputs;
+
+// import React, { useState, ChangeEvent } from 'react';
+
+// import s from './inputs.module.scss';
+
+// const Inputs = () => {
+//   const [sortBy, setSortBy] = useState('');
+//   const [findBy, setFindBy] = useState('');
+//   const [filterBy, setFilterBy] = useState('');
+
+//   const sortChange = (event: ChangeEvent<HTMLSelectElement>) => {
+//     setSortBy(event.target.value);
+//   };
+
+//   const findChange = (event: ChangeEvent<HTMLInputElement>) => {
+//     setFindBy(event.target.value);
+//   };
+
+//   const filterChange = (event: ChangeEvent<HTMLInputElement>) => {
+//     setFilterBy(event.target.value);
+//   };
+
+//   return (
+//     <div className={s.inputs_wrapper}>
+//       <div className={s.inputs}>
+//         <select value={sortBy} onChange={sortChange} className={s.input + ' ' + s.input_sort}>
+//           <option value=''>Sort by</option>
+//           <option value='price-asc'> &#x25B2; Price (Low to High)</option>
+//           <option value='price-desc'>
+//             <img src={require('../../../../assets/png/sort-arrow.png')} alt='Down Arrow' />
+//             Price (High to Low)
+//           </option>
+//         </select>
+
+//         <div className={s.input_wrapper}>
+//           <input
+//             type='text'
+//             placeholder='Find'
+//             value={findBy}
+//             onChange={findChange}
+//             className={s.input + ' ' + s.input_find}
+//           />
+//         </div>
+//         {/* <div className={s.input_find_after}></div> */}
+
+//         <div className={s.input_wrapper}>
+//           <input
+//             type='text'
+//             placeholder='Filter by'
+//             value={filterBy}
+//             onChange={filterChange}
+//             className={s.input + ' ' + s.input_filter}
+//           />
+//           <div className={s.input_filter_after}></div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Inputs;
