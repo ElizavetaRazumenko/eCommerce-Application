2e475ffdd7f17aa475a817964ca042c83146d838
Uladimir Hrydzin <hrydzin@gmail.com>
2023-09-15 04:10:06 +0400

    fix: section promo

--- src/pages/2.MainPage/components/promo/promo.tsx
+++ src/pages/2.MainPage/components/promo/promo.tsx
@@ -5,16 +5,32 @@ import s from './promo.module.scss';
 const SectionPromo = () => {
   const [promoCode, setPromoCode] = useState('');
   const [isValidPromo, setIsValidPromo] = useState(false);
+  const [selectedPromoCode, setSelectedPromoCode] = useState<{
+    code: string;
+    description: string;
+  } | null>(null);
 
   const handlePromoCodeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.currentTarget.value;
     setPromoCode(value);
+    setSelectedPromoCode(null);
     setIsValidPromo(false);
   };
 
+  const promoCodes = [
+    { code: '246802', description: 'For purchases on even days' },
+    { code: '135791', description: 'For purchases on odd days' },
+    { code: '333333', description: 'For purchases of three or more pizzas' },
+    { code: '444333', description: 'For purchases of three or more beverages' },
+    { code: '555333', description: 'For purchases of three or more sauces' },
+  ];
+
   const applyPromoCode = () => {
-    if (promoCode.length === 6 && /^\d+$/.test(promoCode)) {
+    const selectedPromo = promoCodes.find((promo) => promo.code === promoCode);
+
+    if (selectedPromo) {
       setIsValidPromo(true);
+      setSelectedPromoCode(selectedPromo);
     } else {
       setIsValidPromo(false);
     }
@@ -22,7 +38,24 @@ const SectionPromo = () => {
 
   return (
     <section className={s.section_promo}>
-      <h2 className={s.title_promo}>Promo code:</h2>
+      <h2 className={s.title_promo}>Promo codes:</h2>
+      <div className={s.promo_list}>
+        {promoCodes.map((promo, index) => (
+          <button
+            key={index}
+            className={`${s.btn_select_promo} ${
+              selectedPromoCode?.code === promo.code ? s.selected_promo : ''
+            }`}
+            onClick={() => {
+              setPromoCode(promo.code);
+              setSelectedPromoCode(promo);
+            }}
+          >
+            <div className={s.promo_description}>{promo.description}</div>
+            <div className={s.promo_number}>{promo.code}</div>
+          </button>
+        ))}
+      </div>
       <div className={s.promo_input}>
         <input
           type='text'
@@ -30,14 +63,16 @@ const SectionPromo = () => {
           value={promoCode}
           onChange={handlePromoCodeChange}
         />
-        <button onClick={applyPromoCode} className={s.btn_promo}>
+        <button onClick={applyPromoCode} className={s.btn_apply}>
           Apply
         </button>
       </div>
       {isValidPromo ? (
-        <p className={s.valid_promo}>Get 10% discount!</p>
+        <p className={s.valid_promo}>
+          Applied Promo: {selectedPromoCode?.code} - {selectedPromoCode?.description}
+        </p>
       ) : (
-        <p className={s.invalid_promo}>Promo code must be contain 6 numbers. Please try again.</p>
+        <p className={s.invalid_promo}>Invalid promo code. Please try again.</p>
       )}
     </section>
   );
